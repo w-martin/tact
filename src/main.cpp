df02ab433fd84ff83940b3d063a1b2e7f8d7708c
William Martin <will.st4@gmail.com>
2011-10-08 00:45:39 +0100

    added main file

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * @author  William Martin <will.st4@gmail.com>
+ * @version 0.0
+ *
+ * @section LICENSE
+ *
+ * This program is licensed under the MIT license, please see COPYING for a 
+ * copy of the license.
+ *
+ * @section DESCRIPTION
+ *
+ * This is the main entry point at runtime.
+ * 
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/**
+ * Main entry point for the program.
+ * 
+ * @param argc number of arguments.
+ * @param argv string arguments from <code>stdin</code>.
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
