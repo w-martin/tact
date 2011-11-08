f53b137d0d9b7c354c4f73b25f5a81a99ef4c9b8
William Martin <will.st4@gmail.com>
2011-11-08 07:35:12 +0000

    added lda file

---
+++ src/algorithm/LatentDirichletAllocation.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   LatentDirichletAllocation.cpp
+ * Author: will
+ * 
+ * Created on November 6, 2011, 6:24 PM
+ */
+
+#include "LatentDirichletAllocation.h"
+
+LatentDirichletAllocation::LatentDirichletAllocation() {
+}
+
+LatentDirichletAllocation::LatentDirichletAllocation(const LatentDirichletAllocation& orig) {
+}
+
+LatentDirichletAllocation::~LatentDirichletAllocation() {
+}
+
