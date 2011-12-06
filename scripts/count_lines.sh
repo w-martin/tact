c857c69c619e9b2cda9b5488a34b69287419c4b3
William Martin <will.st4@gmail.com>
2011-12-06 22:25:28 +0000

    greatly improved line counting

--- scripts/count_lines.sh
+++ scripts/count_lines.sh
@@ -1,4 +1,24 @@
 #! /bin/sh
 # Counts the lines of code in this project (excluding included libraries).
+# example usage: count_lines.sh ./src ./test ./include ./scripts --c
+# example usage: count_lines.sh --c --d
+#
+defaultLocations="./src ./test ./include ./scripts"
+locations=""
+comments=""
+for i in "$@"
+do
+    if [ "$i" == "--help" ]; then
+        echo "\nUsage: count_lines.sh directory1 directory2...options"
+        echo "Options:\n    --c Ignore comments\n    --d Default locations\n"
+        exit
+    elif [ "$i" == "--c" ]; then
+    	comments="/^\s*#/d;/^\s*$/d;/^\s*\/\//d"
+    elif [ "$i" == "--d" ]; then
+    	locations="${locations} $defaultLocations"
+    else
+    	locations="${locations} $i"
+    fi
+done
 
-( find ./src ./test ./include ./scripts -name '*.*' -print0 | xargs -0 cat ) | sed '/^\s*#/d;/^\s*$/d;/^\s*\/\//d' | wc -l
+( find $locations -type f -print0 | xargs -0 cat ) | sed "$comments" | wc -l
