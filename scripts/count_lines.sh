8b06c391aaf576506c3b2ecad0db7576d195bd44
William Martin <will.st4@gmail.com>
2011-12-06 17:00:46 +0000

    included scripts directory in count

--- scripts/count_lines.sh
+++ scripts/count_lines.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 # Counts the lines of code in this project (excluding included libraries).
 
-( find ./src ./test ./include -name '*.*' -print0 | xargs -0 cat ) | wc -l
+( find ./src ./test ./include ./scripts -name '*.*' -print0 | xargs -0 cat ) | wc -l
