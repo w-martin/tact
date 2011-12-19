dea0424e9cfdc5fb4723734cafb8a83cb26f3ed3
William Martin <will.st4@gmail.com>
2011-12-19 16:41:07 +0000

    began defining topic model worker

--- include/tmte-cpp/algorithm/ParallelTopicModelWorker.h
+++ include/tmte-cpp/algorithm/ParallelTopicModelWorker.h
@@ -13,13 +13,89 @@
 #ifndef PARALLELTOPICMODELWORKER_H
 #define	PARALLELTOPICMODELWORKER_H
 
+#include "tmte-cpp/output/TopicAssignment.h"
+#include <vector>
+
+using std::vector;
+
 class ParallelTopicModelWorker {
 public:
-    ParallelTopicModelWorker();
+    /**
+     * Default constructor.
+     * 
+     * @param noTopics The number of topics.
+     * @param alpha The alpha distribution over topics.
+     * @param alphaSum The sum of the alpha distribution.
+     * @param beta The prior on the per-topic multinomial distribution over 
+     * words.
+     * @param topicAssignments The initial topic assignments.
+     * @param typeTopicCounts The topic counts for each feature.
+     * @param tokensPerTopic The array of tokens per topic.
+     * @param startDocument The starting document for this Worker.
+     * @param noDocuments The total number of documents in the input space.
+     * 
+     */
+    ParallelTopicModelWorker(
+            int noTopics,
+            double * alpha,
+            double alphaSum,
+            double beta,
+            vector<TopicAssignment*> * topicAssignments,
+            int * * typeTopicCounts,
+            int * tokensPerTopic,
+            int startDocument,
+            int noDocuments);
     ParallelTopicModelWorker(const ParallelTopicModelWorker& orig);
     virtual ~ParallelTopicModelWorker();
-private:
-    
+protected:
+    vector<TopicAssignment*> * topicAssignments;
+    int startDocument;
+    int noDocuments;
+    int noTopics;
+    /**
+     * Dirichlet(alpha,alpha,...) is the distribution over topics.
+     * 
+     */
+    double * alpha;
+    double alphaSum;
+    /**
+     * Prior on per-topic multinomial distribution over words.
+     * 
+     */
+    double beta;
+    double betaSum;
+    /******************************************************
+     * These values are used to encode type/topic counts as
+     * count/topic pairs in a single int.
+     */
+    int topicMask;
+    int topicBits;
+    int noTypes;
+    /**
+     * Indexed by [feature index, topic index].
+     * 
+     */
+    int * * typeTopicCounts;
+    /**
+     * Indexed by topic.
+     * 
+     */
+    int * tokensPerTopic;
+    /******************************************************
+     * Used for dirichlet estimation.
+     * 
+     */
+    /**
+     * Histogram of document lengths.
+     * 
+     */
+    int * documentLengthCounts;
+    /**
+     *  Histogram of document/topic counts, indexed by: 
+     * [topic index, sequence position index].
+     * 
+     */
+    int * * topicDocumentCounts;
 };
 
 #endif	/* PARALLELTOPICMODELWORKER_H */
--- include/tmte-cpp/model/TopicModel.h
+++ include/tmte-cpp/model/TopicModel.h
@@ -141,4 +141,3 @@ protected:
 };
 
 #endif	/* TOPICMODEL_H */
-
