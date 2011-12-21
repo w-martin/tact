bc49249eb26555214a7e9abdf62e76191c4e37c8
William Martin <will.st4@gmail.com>
2011-12-21 00:20:24 +0000

    continued porting parallel evaluate method

--- src/algorithm/ParallelTopicModel.cpp
+++ src/algorithm/ParallelTopicModel.cpp
@@ -11,6 +11,7 @@
  */
 
 #include "tmte-cpp/algorithm/ParallelTopicModel.h"
+#include "tmte-cpp/algorithm/ParallelTopicModelWorker.h"
 #include <time.h>
 #include <iostream>
 #include <pthread.h>
@@ -39,10 +40,41 @@ int const ParallelTopicModel::getNoThreads() const {
 void ParallelTopicModel::estimate() {
     long startTime = time(NULL);
     int const noDocumentsPerThread = topicAssignments->size() / noThreads;
+    // create worker objects
+    vector<ParallelTopicModelWorker*> * const workers
+            = new vector<ParallelTopicModelWorker*>();
+    int startingDocument = 0;
+    int * * tokensPerTopic = new int*[noThreads];
+    int * * * typeTopicCounts = new int**[noThreads];
+    
+    for (int i = 0; i < noThreads; i++) {
+        
+        tokensPerTopic[i] = new int[noTopics];
+        for(int j=0;j<noTopics;j++){
+//            tokensPerTopic[i][j] = ParallelTopicModel::tokensPerTopic[j];
+        }
+        
+//        typeTopicCounts[i] = new int*[noTypes];
+//        for(int j=0;j<noTypes;j++){
+//        typeTopicCounts[i][j] = new int[]
+//        }
+                
+        startingDocument += noDocumentsPerThread;
+    }
+    // start threads
     pthread_t * threads = new pthread_t[noThreads];
     for (int i = 0; i < noThreads; i++) {
         
     }
+    // wait on threads
+    // evaluate
+
+    // free memory
+    for (int i = 0; i < noThreads; i++) {
+        delete workers->at(i);
+    }
+    delete workers;
+    delete threads;
 }
 
 //void * thread(void *argv) {
